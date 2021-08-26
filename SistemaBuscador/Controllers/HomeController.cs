21fd17699847db6862225150546331502e5dc73e
Juan Enrique Cabreara Reyes <juan.cabrerar@correoaiep.cl>
2021-08-26 18:46:36 -0400

    clase 2 OK

--- SistemaBuscador/Controllers/HomeController.cs
+++ SistemaBuscador/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace SistemaBuscador.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+            return View("Privacy");
+        }
+
         public IActionResult Prueba()
         {
             return View();
